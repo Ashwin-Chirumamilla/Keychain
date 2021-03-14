fd138252597646018b2461edc813f82028e8f97e
Ashwin Chirumamilla <ashwinchirumamilla@gmail.com>
2021-03-14 08:32:35 -0500

    changes

--- app.js
+++ app.js
@@ -4,23 +4,29 @@ const port = 3030;
 const axios = require('axios');
 let stocks = ["AAPL", "MSFT", "AMZN", "FB", "TSLA"];
 const API_key = 'c16jep748v6ppg7est50';
-let quotes = {}
+
 
 app.listen(port, () => {
   console.log(`Notifications service listening on https://localhost:${port}`);
 });
 
 app.get('/', (req, res) => {
+  let quotes = [];
+  
   stocks.forEach(element => {
     axios.get(`https://finnhub.io/api/v1/quote?symbol=${element}&token=${API_key}`)
-    .then(response => {
-      console.log(element);
-      console.log(response.data);
-
+    .then((response) => {
+      
+        quotes.push(response.data);
+        console.log(quotes);
+        res.json(quotes);
     })
     .catch(error => {
       console.log(error);
-
     });
+    
   });
+  
+  
 });
+
